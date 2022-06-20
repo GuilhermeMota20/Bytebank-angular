b3bddc8dee330e3c840cc1aadffd440c1bb602df
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-06-20 12:10:12 -0300

    FT - working with ng models, events and data

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTransferenciaComponent { }
+export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitando nova transferencia');
+    console.log('Valor:', this.valor);
+    console.log('Destino:', this.destino);
+  };
+
+}
